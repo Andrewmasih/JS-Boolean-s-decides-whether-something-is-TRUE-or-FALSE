fd8daad523e86955b7aea78d535edb041558d5b0
Andrew Masih <armasih@outlook.com>
2021-06-01 21:55:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/* by assigning the TRUE or FALSE, ON or OFF, YES or NO, to an object */
+
+let booleanTrue = true;
+let booleanFalse = false;
+
+console.log(booleanTrue, booleanFalse);
+
+/* 'comparator' operators, they return a boolean value: either TRUE or FALSE  */
+
+console.log(5<10);
+console.log(10<5);
+console.log(1==1);
+console.log(1!=2);
+console.log("banana" == "banana");
+
